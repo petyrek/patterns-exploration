93fd037c199c901de2d23f550e90fc69e2fd51ed
unknown <petyrekm@gmail.com>
2019-02-20 16:16:43 +0100

    fix brand list component

--- src/components/BrandList/index.js
+++ src/components/BrandList/index.js
@@ -1,13 +1,14 @@
 import React from "react"
 import { withCollection } from "hocs/withCollection"
 
-const C = ({ state }) =>
-  state.map(data => (
+const enhancer = withCollection("makes")
+
+export const BrandList = enhancer(({ stateU }) =>
+  stateU.map(data => (
     <ul>
       {data.map(x => (
         <li key={x.id}>{x.name}</li>
       ))}
     </ul>
-  ))
-
-export const BrandList = withCollection("makes")(C)
+  )),
+)
